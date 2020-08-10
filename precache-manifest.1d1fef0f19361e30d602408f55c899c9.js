83864426948fc8f4e1db85d2e14ec99bc5025bff
Amakoye <charlesamakoye@gmail.com>
2020-08-10 22:25:23 +0300

    Updates

---
+++ precache-manifest.1d1fef0f19361e30d602408f55c899c9.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6182151e00aa6da7b831366733e3e625",
+    "url": "/voting_app/index.html"
+  },
+  {
+    "revision": "32137390ea13e5b34f6b",
+    "url": "/voting_app/static/css/2.15a2e2b7.chunk.css"
+  },
+  {
+    "revision": "32137390ea13e5b34f6b",
+    "url": "/voting_app/static/js/2.00672495.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/voting_app/static/js/2.00672495.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf65382362d291ca4ef3",
+    "url": "/voting_app/static/js/main.7b6a511e.chunk.js"
+  },
+  {
+    "revision": "fbe40b2608630a2b1411",
+    "url": "/voting_app/static/js/runtime-main.3e1efd19.js"
+  },
+  {
+    "revision": "ea8715646795e6492859745b5cdffe7b",
+    "url": "/voting_app/static/media/avatar1.ea871564.png"
+  },
+  {
+    "revision": "085724013c40fa4e4c254e26538812bc",
+    "url": "/voting_app/static/media/avatar2.08572401.png"
+  },
+  {
+    "revision": "22858dfa644bb4d45ee6afb4b4f3239b",
+    "url": "/voting_app/static/media/avatar3.22858dfa.png"
+  },
+  {
+    "revision": "955d77c3d7a1f8e0cf67042c5180e772",
+    "url": "/voting_app/static/media/avatar4.955d77c3.png"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/voting_app/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/voting_app/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/voting_app/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/voting_app/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/voting_app/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/voting_app/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/voting_app/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/voting_app/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/voting_app/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/voting_app/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/voting_app/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/voting_app/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/voting_app/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/voting_app/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/voting_app/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/voting_app/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "4e98389dab224eccd1103846fe121405",
+    "url": "/voting_app/static/media/product1.4e98389d.png"
+  },
+  {
+    "revision": "7734de39f2c3c649fb726a3e9e40bba1",
+    "url": "/voting_app/static/media/product3.7734de39.png"
+  },
+  {
+    "revision": "0022b204c884a79ac7e70f14ab722768",
+    "url": "/voting_app/static/media/product4.0022b204.png"
+  }
+]);
